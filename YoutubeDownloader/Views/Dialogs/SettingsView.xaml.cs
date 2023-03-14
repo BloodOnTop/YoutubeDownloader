22cdd9d6e51b9493dc13d655e92079cfba5d3022
Blood <78785435+BloodOnTop@users.noreply.github.com>
2023-03-14 14:17:49 +0100

    update file

---
+++ YoutubeDownloader/Views/Dialogs/SettingsView.xaml.cs
@@ -0,0 +1,17 @@
+using System.Windows;
+
+namespace YoutubeDownloader.Views.Dialogs;
+
+public partial class SettingsView
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+
+    private void DarkModeToggleButton_Checked(object sender, RoutedEventArgs args) =>
+        App.SetDarkTheme();
+
+    private void DarkModeToggleButton_Unchecked(object sender, RoutedEventArgs args) =>
+        App.SetLightTheme();
+}
