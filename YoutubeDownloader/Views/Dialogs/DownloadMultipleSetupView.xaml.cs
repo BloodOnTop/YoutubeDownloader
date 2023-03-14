494ce39c97f3e4142152c56dd6b9e3f7c770f02e
Blood <78785435+BloodOnTop@users.noreply.github.com>
2023-03-14 14:17:45 +0100

    update file

---
+++ YoutubeDownloader/Views/Dialogs/DownloadMultipleSetupView.xaml.cs
@@ -0,0 +1,9 @@
+namespace YoutubeDownloader.Views.Dialogs;
+
+public partial class DownloadMultipleSetupView
+{
+    public DownloadMultipleSetupView()
+    {
+        InitializeComponent();
+    }
+}
