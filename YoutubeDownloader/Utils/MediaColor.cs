e3e087ca8e6d2eafd918254e4ccd2eeabc200a96
Blood <78785435+BloodOnTop@users.noreply.github.com>
2023-03-14 14:17:35 +0100

    update file

---
+++ YoutubeDownloader/Utils/MediaColor.cs
@@ -0,0 +1,8 @@
+using System.Windows.Media;
+
+namespace YoutubeDownloader.Utils;
+
+internal static class MediaColor
+{
+    public static Color FromHex(string hex) => (Color) ColorConverter.ConvertFromString(hex);
+}
